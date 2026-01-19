cbae245f360226cec92eb18bc6a29baee2eff5f5
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-20 03:22:10 +0530

    chore:discriminated and exhaustiveness

--- Week1/detection.ts
+++ Week1/detection.ts
@@ -37,3 +37,45 @@ function getFood(pet: Fish | Bird){
 
 //basically what happening is that over here first we check that if a pet is fish or bed using isFish function
 //then we are selecting our food according to that function value if its true or false
+
+//____DISCRIMINATED_UNION___
+interface Circle{
+    kind: 'circle',
+    radius: number
+}
+
+interface Square{
+    kind:'square',
+    side:number
+}
+
+interface Rectangle{
+    kind:'rectangle',
+    length:number,
+    width:number
+}
+
+type Shape  = Circle | Square | Rectangle
+
+function  getTrueShape(shape:Shape){
+    if(shape.kind === "circle"){
+        return Math.PI * shape.radius ** 2
+    }
+    // return shape.side * shape.side
+}
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case 'circle':
+            return Math.PI * shape.radius ** 2
+        case 'square':
+            return shape.side * shape.side
+        case 'rectangle':
+            return shape.length * shape.width
+        default:
+            const _randomvariable: never = shape   //always have a default case assign to never so that it can catch a scene where we dont have case in the switch for a specific case
+            return _randomvariable
+    }
+}
+
+
