3fe7717d51a5d8794daac69f9ff506826c2035d8
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-19 20:18:27 +0530

    chore:protected

--- Week1/index2.ts
+++ Week1/index2.ts
@@ -1,5 +1,6 @@
 class taser{
     private _courseCount = 1
+    protected count = 1
     email:string
     private readonly city: string = 'Jaipur'
     constructor(
@@ -24,6 +25,17 @@ class taser{
     private deleteToken(){
         console.log('token deleted') //private methods
     }
+}
+// supose you have to inherit a class 
+class animal extends taser{
+    isfamily:boolean
+    getcoursecount(){
+        this._courseCount //this will give an error so lets take xample for variable x and assign it protected then you can access that variable in its class as well as its subclass
+    }
+    getcount(){
+        this.count //see it will not give an error
+    }
+
 }
 
 export{}
